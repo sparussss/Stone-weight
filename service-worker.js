692f81123761c39fe08007b5fe0f8b194764a93e
KL YANG <122142355+sparussss@users.noreply.github.com>
2026-07-12 02:51:24 +0800

    Stone_Weight_PWA_1.2.0

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,5 @@
-const CACHE='stone-weight-pwa-1-1-6';
-const ASSETS=['./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(
-    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const request=event.request;
-  if(request.mode==='navigate' || request.destination==='document'){
-    event.respondWith(
-      fetch(request,{cache:'no-store'})
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-          return response;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then(cached=>{
-      const network=fetch(request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(request,copy));
-        return response;
-      });
-      return cached || network;
-    })
-  );
-});
+const CACHE='stone-weight-pwa-1-2-0';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return;}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});

5bc6a23abb4bda63f2014f15f962e54d5ba207f4
KL YANG <122142355+sparussss@users.noreply.github.com>
2026-07-12 05:22:56 +0800

    Stone_Weight_PWA_1.2.5_COMPLETE

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,22 @@
-const CACHE='stone-weight-pwa-1-2-4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+const CACHE = 'stone-weight-pwa-1-2-5-complete';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install',event=>{
-  event.waitUntil(
-    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
-self.addEventListener('fetch',event=>{
-  const request=event.request;
-  event.respondWith(
-    caches.match(request).then(cached=>{
-      if(cached) return cached;
-      return fetch(request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(request,copy));
-        return response;
-      });
-    })
-  );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => {
+    if (cached) return cached;
+    return fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    });
+  }));
 });

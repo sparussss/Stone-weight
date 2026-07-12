1266235c56e711a3357bf66f1dc08043b0aad8c6
KL YANG <122142355+sparussss@users.noreply.github.com>
2026-07-12 13:55:37 +0800

    Stone_Weight_PWA_1.2.7_COMPLETE_FIXED

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'stone-weight-pwa-1-2-6-complete';
+const CACHE = 'stone-weight-pwa-1-2-7-complete';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
